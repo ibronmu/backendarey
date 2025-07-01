85dab52d28b62ea63eeef937569fb752f7c7d45b
ibronmu <ibronmu@gmail.com>
2025-07-01 11:49:04 -0700

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import userRoutes from './routes/users.js';
-import { body, validationResult } from 'express-validator';
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
-
+app.get('/',(req,res)=>{
+  res.send("Hello, World")
+   
+})
 app.use(userRoutes);
 
 app.use((req, res) => {
